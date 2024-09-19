19f923ce0eed91bdc450647cdff2586a94b789d4
Nihal Varma <nihalvarma100@gmail.com>
2024-09-19 16:00:26 +0530

    Adding Flies

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount]=useState(0);
+
+  return (
+    <div className='App'>
+      <header className='header'>
+        <h1>Local Counter App</h1>
+      </header>
+
+      <h2>count: {count}</h2>
+      <button onClick={()=> setCount(0)}>RESET</button>
+      <button onClick={()=> count>=15? setCount(0): setCount(count+1)}>INCREASE</button>
+      <button onClick={()=> count<=0? setCount(0): setCount(count-1)}>DECREASE</button>
+    </div>
+  )
+}
+
+export default App;
